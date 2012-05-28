b5749a6eff6dca20c0b718c66f23eac0cff26b9c
Kolyvan <ru.kolyvan@gmail.com>
2012-05-28 20:33:59 +1100

    initial commit

---
+++ iSamizdat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iSamizdat
+//
+//  Created by Kolyvan on 28.05.12.
+//  Copyright (c) 2012 Konstantin Boukreev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UINavigationController *navigationController;
+
+@end
---
+++ iSamizdat/AuthorsViewController.h
@@ -0,0 +1,14 @@
+//
+//  AuthorsViewController.h
+//  iSamizdat
+//
+//  Created by Kolyvan on 28.05.12.
+//  Copyright (c) 2012 Konstantin Boukreev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AuthorsViewController : UITableViewController
+
+@end
