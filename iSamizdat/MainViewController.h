b3700735d3ec58a21bca82f64fa0a679213baa1b
Kolyvan <ru.kolyvan@gmail.com>
2012-05-30 02:02:25 +1100

    added a remark about licensed under lgpl v3

--- iSamizdat/MainViewController.h
+++ iSamizdat/MainViewController.h
@@ -5,6 +5,10 @@
 //  Created by Kolyvan on 28.05.12.
 //  Copyright (c) 2012 Konstantin Boukreev. All rights reserved.
 //
+//  https://github.com/kolyvan/iSamizdat
+//  this file is part of iSamizdat
+//  Samizdat is licenced under the LGPL v3, see lgpl-3.0.txt
+// 
 
 #import <UIKit/UIKit.h>
 #import "SSPullToRefresh.h"
--- iSamizdat/SamLibText+IOS.h
+++ iSamizdat/SamLibText+IOS.h
@@ -5,6 +5,11 @@
 //  Created by Kolyvan on 30.05.12.
 //  Copyright (c) 2012 Konstantin Boukreev . All rights reserved.
 //
+//  https://github.com/kolyvan/iSamizdat
+//  this file is part of iSamizdat
+//  Samizdat is licenced under the LGPL v3, see lgpl-3.0.txt
+// 
+
 
 #import "SamLibText.h"
 
