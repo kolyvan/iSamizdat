81d22ae8e102af71f4b6770df6169a557d8999ad
Kolyvan <ru.kolyvan@gmail.com>
2012-07-24 18:52:33 +1100

    improved UserView

--- iSamizdat/UserViewController.m
+++ iSamizdat/UserViewController.m
@@ -307,6 +307,9 @@ titleForHeaderInSection:(NSInteger)section
     return cell;
 }
 
-#pragma mark - Table view delegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+     [self.tableView deselectRowAtIndexPath:indexPath animated:NO];
+}
 
 @end
